66e2737296ee706e1d2d626e111ab51b58a65ff5
Yura <bobr515yura@yandex.ru>
2023-11-17 13:17:29 +0300

    Mywork

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let d = Math.pow(b, 2) - 4 * a * c;
+    let result;
+    if (d<0) {
+        result = [];
+    } else if (d===0) {
+        result = [(-b / (2 * a))] ;
+    } else {
+        result = [((-b + Math.sqrt(d) )/(2*a)), ((-b - Math.sqrt(d) )/(2*a))];
+
+    }
+    return result;
+    
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let translationPercent = (percent/100)/12;
+  let bodyCredit =  amount - contribution;
+  let monthPay = bodyCredit * (translationPercent + (translationPercent / (( Math.pow((1 + translationPercent), countMonths)) - 1)));
+  let result = monthPay*countMonths;
+  return +result.toFixed(2);
 }
