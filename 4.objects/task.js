54f571cb615391288818fa83adbd2f326029674d
Yura <bobr515yura@yandex.ru>
2023-11-29 22:25:40 +0300

    Mytask

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name=name;
+  this.gender=gender;
+  this.age=age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subjectName=subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+  if (marks.length==0) {
+    this.marks="Студент отчислен";
+  } else {
+    this.marks=marks;
+  }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(this.marks === undefined){
+        return 'Нет оценок';
+    } else {
+        return this.marks.reduce((acc,item)=>acc+item, 0)/this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded=reason;
 }
