32d65b0d4571e8f634673d6abc9adcc791ab94c9
Yura <bobr515yura@yandex.ru>
2023-12-07 13:53:51 +0300

    Mytask

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,6 +2,7 @@ function Student(name, gender, age) {
   this.name=name;
   this.gender=gender;
   this.age=age;
+  this.marks=[];
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -9,20 +10,18 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (marks.length==0) {
-    this.marks="Студент отчислен";
-  } else {
-    this.marks=marks;
-  }
+  if (marks.length>0) {
+    this.marks.push(marks);
+  } 
   
 }
 
 Student.prototype.getAverage = function () {
-    if(this.marks === undefined){
-        return 'Нет оценок';
-    } else {
-        return this.marks.reduce((acc,item)=>acc+item, 0)/this.marks.length;
-    }
+    if(this.marks === undefined||this.marks.length===0) {
+        return 0;
+    } 
+    return this.marks.reduce((acc,item)=>acc+item, 0)/this.marks.length;
+    
 }
 
 Student.prototype.exclude = function (reason) {
